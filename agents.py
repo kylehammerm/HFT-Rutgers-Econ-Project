fd935f308991f912428159d2b3870103cf0491d4
Kyle Hammermueller <kylehammerm@gmail.com>
2025-04-02 20:49:23 -0400

    Created basic map with sugar and spice

--- agents.py
+++ agents.py
@@ -7,8 +7,13 @@ class Sugar(mesa.Agent):
         -grows one amount of sugar at each turn
     '''
 
-    def __init__(self):
-        print("I am sugar")
+    def __init__(self, unique_id, model, pos, max_sugar):
+        super().__init__(model)  
+        self.pos = pos
+        self.amount = max_sugar
+        self.max_sugar = max_sugar
+
+
 
 
 class Spice(mesa.Agent):
@@ -19,8 +24,12 @@ class Spice(mesa.Agent):
     
     '''
 
-    def __init__(self):
-        print("I am spice")
+    def __init__(self,unique_id, model, pos, max_spice):
+        super().__init__(model)
+        self.pos = pos
+        self.amount = max_spice
+        self.max_spice = max_spice
+        
 
 
 class Trader(mesa.Agent):
